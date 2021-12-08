6d191b6e4ec825a3e7c3ee3991eedb6debf2a460
Alexandre B. L <alexandrebl@protonmail.com>
2021-12-08 21:24:05 +0000

    08/12/2021 21:24:05

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+$root    = ""; 
+$db_user = "";
+$db_pass = "";
+
+?>
--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <?php
 
+require_once "db.php";
+
 setcookie("loged_in", false, time() + 86400, "/");
 
-if( !empty( $_COOKIE["loged_in"] ) ) {
+if( empty( $_COOKIE["loged_in"] ) ) {
 
-    echo $_COOKIE["loged_in"];
+    header('Location: '.$db.'/login.php');
 
-}
+} else { ?>
 
-?>
+    <h1> Welcome back </h1>
+
+<?php } ?>
