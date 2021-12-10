9cdb0d06a67fb92e21a35da6ada7c80cd7a7098b
Alexandre B. L <alexandrebl@protonmail.com>
2021-12-10 11:46:38 +0000

    10/12/2021 11:46:38

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ if( empty( $_COOKIE["loged_in"] ) ) {
     <body>
         <h1> Welcome back </h1>
         <?php echo exec('
-            curl "https://api.vultr.com/v2/account" \
+            curl "https://api.vultr.com/v2/products" \
             -X GET \
             -H "Authorization: Bearer '.$G_api.'"
         '); ?>        
