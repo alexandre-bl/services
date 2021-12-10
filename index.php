4fa24833732a59e7e17b65bf4579bde3d733e5b2
Alexandre B. L <alexandrebl@protonmail.com>
2021-12-10 12:23:54 +0000

    10/12/2021 12:23:54

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ $results = json_decode( exec($cmd) );
         <?php foreach( $results->domains as $domain ) { ?>
             <div class="domain">
                 <h2 class="title"><?php echo $domain->domain; ?>      </h2>
-                <p  class="date"> <?php echo $domain->date_created; ?></p>
+                <p  class="date"> <?php echo $domain->date_created[0:9]; ?></p>
             </div>
         <?php }; ?>        
     </body>
