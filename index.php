f96bc29c8ff537d605953cbc9057484f4204f32b
Alexandre B. L <alexandrebl@protonmail.com>
2021-12-10 23:11:33 +0000

    10/12/2021 23:11:33

--- index.php
+++ index.php
@@ -25,6 +25,8 @@ foreach( $results->domains as $domain ) {
     <head>
 
         <link rel="stylesheet" href="style.css">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js">
+    </script>
 
     </head>
 
