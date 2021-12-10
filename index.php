7ab1d75e39686db21d3be0703fd9632e634fb48c
Alexandre B. L <alexandrebl@protonmail.com>
2021-12-10 12:15:52 +0000

    10/12/2021 12:15:52

--- index.php
+++ index.php
@@ -25,7 +25,6 @@ $results = json_decode( exec($cmd) );
     </head>
 
     <body>
-        <h1> Welcome back </h1>
         <?php foreach( $results->domains as $domain ) { ?>
             <div class="domain">
                 <h2 class="title"><?php echo $domain->domain; ?>      </h2>
