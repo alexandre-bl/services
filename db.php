1d42558d350c27e6af36ae5f24b8f5bffd0ac035
Alexandre B. L <alexandrebl@protonmail.com>
2021-12-11 20:53:51 +0000

    11/12/2021 20:53:51

--- db.php
+++ db.php
@@ -6,6 +6,15 @@ if( $conn->connect_error ) {
     die("Connection to database failed: " . $conn->connect_error);
 }
 
-echo "Connected to database."
+echo "Connected to database"
 
-?>
+$conn->query(
+    "CREATE IF NOT EXISTS services"
+);
+if ($conn->query($sql) === TRUE) {
+    echo "Database created successfully";
+} else {
+    echo "Error creating database: " . $conn->error;
+}
+
+$conn->close(); ?>
