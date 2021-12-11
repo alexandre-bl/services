27028e47ebbef611807fe2691de5f2d0bb218aab
Alexandre B. L <alexandrebl@protonmail.com>
2021-12-11 18:57:42 +0000

    11/12/2021 18:57:42

--- index.php
+++ index.php
@@ -30,18 +30,18 @@ foreach( $results->domains as $domain ) {
     <head>
 
         <link rel="stylesheet" href="style.css">
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js"></script>
         <script type="text/javascript">
             function showHideRow(row) {
                 $("." + row).toggle();
             }
-        </script>
+        </script> -->
 
     </head>
 
     <body>
-        <table id="domains">
+        <!--<table id="domains">
             <tr>
                 <th>0</th>
                 <th>1</th>
@@ -72,5 +72,5 @@ foreach( $results->domains as $domain ) {
                     </tr>
                 <?php } ?>
             <?php $i++; }; ?>
-        </table>  
+        </table> -->
     </body>
