2afe02eb9095596a08b801500f9abd1907541bb1
Alexandre B. L <alexandrebl@protonmail.com>
2021-12-20 19:37:40 +0000

    20/12/2021 19:37:40

--- blocks/navbar.php
+++ blocks/navbar.php
@@ -1,6 +1,6 @@
 <div id="navbar">
-    <a href="/?logout=1">     Logout  </a>
-    <a href="/p/storage.php"> Storage </a>
-    <a href="/p/scripts.php"> Scripts </a>
+    <a href="/?logout=1">Logout</a>
+    <a href="/p/storage.php">Storage</a>
+    <a href="/p/scripts.php">Scripts</a>
     <p> <?php require_once "db.php"; ?> </p>
 </div>
