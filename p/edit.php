323ace9315bedd5efb25d1ab12d0e5f64109f482
Alexandre B. L <alexandrebl@protonmail.com>
2021-12-20 22:42:55 +0000

    20/12/2021 22:42:55

--- p/edit.php
+++ p/edit.php
@@ -14,27 +14,6 @@
 
         <?php require_once "../blocks/navbar.php"; ?>
         
-        <div id="content"> <?php
-
-            if( empty( $_GET["table"] ) or empty( $_GET["row"] ) ) { ?>
-
-                <h2 class="error"> Error 400: Bad Request. </h2>
-
-            <?php }
-            else { 
-                
-                $table_is = false;
-                $table = -1;
-                foreach( $Q_tables as $ ) {
-                    if( $_GET["table"] == $t[0] ) {
-                        $table_is = true;
-                        $table = $t;
-                    }
-                }
-
-
-            }
-
-        ?> </div>
+        <div id="content"> </div>
 
     </body>
