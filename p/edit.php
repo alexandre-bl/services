599568e3a9f7a10ec944ccedb9e2a3fe22fef973
Alexandre B. L <alexandrebl@protonmail.com>
2021-12-29 19:52:14 +0000

    29/12/2021 19:52:14

--- p/edit.php
+++ p/edit.php
@@ -40,7 +40,7 @@
             echo $sql."<br>";
 
             $row = (int)$_POST["row"];
-            $res = $conn->query( "SELECT * FROM domains WHERE id IN($row)" )->fetch_assoc();
+            $res = $conn->query( "SELECT * FROM domains WHERE id IN($row)" );
 
             print_r($res);
 
